a13971d4ebddca319cf8295ae169abb122a8be4a
jeewhan <jeewhan@cmykpluswhite.com>
2013-09-26 16:18:27 -0400

    test

--- 404.php
+++ 404.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * The template for displaying 404 pages (Not Found).
- * cal function "wp_is_mobile_not_table" in the vars.php 
+ * cal function "wp_is_mobile_not_table" in the vars.php
+ * 
  * @package RA
  */
 if ( wp_is_mobile_not_tablet() ) 
