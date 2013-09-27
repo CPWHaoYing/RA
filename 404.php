4ad606b697855e950926e560f42ac1571e8f953b
jeewhan <jeewhan@cmykpluswhite.com>
2013-09-27 09:52:57 -0400

    fixed by Qi

--- 404.php
+++ 404.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * The template for displaying 404 pages (Not Found).
- * cal function "wp_is_mobile_not_table" in the vars.php
- * ..
+ * cal function "wp_is_mobile_not_table" in the vars.php 
  * @package RA
  */
 if ( wp_is_mobile_not_tablet() ) 
